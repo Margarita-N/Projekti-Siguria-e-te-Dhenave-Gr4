f0afca31a872c37c56f24e4e159d9d0391d47af4
Margarita-N <58574357+Margarita-N@users.noreply.github.com>
2020-03-20 23:05:24 +0100

    Update NumericalCommand.java

--- Siguria-e-te-Dhenave/NumericalCommand.java
+++ Siguria-e-te-Dhenave/NumericalCommand.java
@@ -1,42 +1,53 @@
-public class NumericalCommand{
+import java.lang.Character;
+public class NumericalCommand {
 	String text;
+	String seperator=" ";
 	
-	public NumericalCommand(String text) {
-		this.text=text;
+	public NumericalCommand() {
+	
 	}
 	
-	public String encrypt() {
+	public void setSeperator(String seperator) {
+		this.seperator=seperator;
+	}
+	
+	public String encrypt(String text) {
+		this.text=text;
 		StringBuilder sb=new StringBuilder();
 		int charToNr;
 		
-		this.text=this.text.replaceAll(" ", "");
+		this.text=this.text.replaceAll(" ","~");
 		this.text=this.text.toUpperCase();
 		
-		for (int i=0;i<this.text.length();i++) {
+		for(int i=0;i<this.text.length();i++) {
 			if(Character.isDigit(this.text.charAt(i)))
 				continue;
 			else
 				charToNr=(int)(this.text.charAt(i)-64);
-	
+			
 			sb.append(charToNr);
-			sb.append(" ");
+			sb.append(this.seperator);
 		}
 		
 		return sb.toString().trim();
 	}
 	
-	public String decrypt() {
+	public String decrypt(String text) {
+		this.text=text;
 		StringBuilder sb=new StringBuilder();
 		char c;
 		
-		String StringArray[]=this.text.split(" ");
+		String StringArray[]=this.text.split(this.seperator);
 		
 		for(int i=0;i<StringArray.length;i++) {
+			if(Integer.parseInt(StringArray[i])==126)
+				c='~';
 			c=(char)(Integer.parseInt(StringArray[i])+64);
 			
 			sb.append(c);
 			
 		}
-		return sb.toString().toLowerCase();
+		return sb.toString().toLowerCase().replaceAll("~"," ");
 	}
 }
+
