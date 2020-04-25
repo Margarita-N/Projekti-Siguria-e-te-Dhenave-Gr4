c9f13e8b3c4b2fc982f47c7d84a8c0469662c932
Margarita-N <58574357+Margarita-N@users.noreply.github.com>
2020-04-25 16:02:25 +0200

    Funksioni per RSA enkriptim

--- Siguria-e-te-Dhenave/PPKeys/ReadWriteMessage.java
+++ Siguria-e-te-Dhenave/PPKeys/ReadWriteMessage.java
@@ -1,5 +1,33 @@
 
 
 public class ReadWriteMessage {
+    String emri;
+    String mesazhi;
+    String path="";
 
+    ReadWriteMessage(String mesazhi){
+        this.mesazhi=mesazhi;
+    }
+
+    public void setPath(String path){
+        this.path=path;
+    }
+
+    public String encrypt(String modulus,String exponent){
+        try{
+            //kthehen ne big integer stringjet e modulit dhe eksponentit
+            RSAPublicKeySpec keySpec= new RSAPublicKeySpec(new BigInteger(modulus),new BigInteger(exponent));
+            KeyFactory keyFactory=KeyFactory.getInstance("RSA");
+            PublicKey celesiPublik=keyFactory.generatePublic(keySpec);
+
+            Cipher cipher=Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            cipher.init(Cipher.ENCRYPT_MODE,celesiPublik);
+
+            byte[] byteCipher=cipher.doFinal(this.mesazhi.getBytes());
+            return Base64.getEncoder().encodeToString(byteCipher);
+        }catch(Exception e) {
+            System.out.println(e.getMessage());
+            return "";
+        }
+    }
 }
