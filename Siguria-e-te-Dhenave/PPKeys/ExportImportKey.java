36bed7ead321723bac3e7ba7356ecc2d75747603
LyraPireva <58956000+LyraPireva@users.noreply.github.com>
2020-04-26 04:26:06 +0200

    Funksioni per exportKey-case private
    Me switch shqyrtohet case "private" (option) krijon nje File objekt me path-in e celesit qe behet export,validimi nese file nuk ekzsiton 
    gjuan exception,ne te kundert permes move() celesi eksportohet ku gjithashtu eshte validuar per te treguar nese eshte ruajt apo jo ne file-in ku eshte bere export

--- Siguria-e-te-Dhenave/PPKeys/ExportImportKey.java
+++ Siguria-e-te-Dhenave/PPKeys/ExportImportKey.java
@@ -1,5 +1,20 @@
 
 
 public class ExportImportKey {
+  public void exportKey(String option){
+        this.option=option;
+        try{
+            switch(this.option){
+                case "private":
+                    File xmlFile=new File("src/PPKeys/keys/"+this.emri+".xml");
+
+                    if(!xmlFile.exists()) throw new FileNotFoundException();
+                    if(this.path=="") throw new InvalidPathException(this.path,"empty");
+
+                    Path moved=Files.move(Paths.get("src/PPKeys/keys/"+this.emri+".xml"),Paths.get(this.path));
+                    if(moved != null) System.out.println("Celesi privat u ruajt ne file-in "+this.path);
+                    else throw new InvalidPathException(this.path,"invalid");
+
+                    break;
 
 }
