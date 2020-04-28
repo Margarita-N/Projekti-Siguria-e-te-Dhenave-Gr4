1cf3bfcebf3a0fe030a52c981cb9cb891ef857ed
Margarita-N <58574357+Margarita-N@users.noreply.github.com>
2020-04-29 00:48:49 +0200

    Rregullimi i parametrave te xmlCreator pas ndryshimit

--- Siguria-e-te-Dhenave/PPKeys/CreateDeleteUser.java
+++ Siguria-e-te-Dhenave/PPKeys/CreateDeleteUser.java
@@ -1,29 +1,25 @@
+package PPKeys;
+
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 
 import javax.naming.NamingException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.math.BigInteger;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.*;
 import java.security.spec.RSAPrivateCrtKeySpec;
+import java.util.Base64;
 
 public class CreateDeleteUser {
-  String emri;
-    CreateDeleteUser(String emri){
+    String emri;
+    public CreateDeleteUser(String emri){
         this.emri=emri;
     }
 
-    private void validimi(String emri) throws NamingException{
+    private void validimi(String emri) throws NamingException {
         char c;
         for(int i=0;i<emri.length();i++){
             c=emri.charAt(i);
@@ -31,15 +27,16 @@ public class CreateDeleteUser {
             else throw new NamingException();
         }
     }
-  //duhet mi gjeneru celsat per me i ru n files
-    private KeyPair keyGenerator() throws Exception{
-            KeyPairGenerator celsat=KeyPairGenerator.getInstance("RSA");
-            celsat.initialize(1024);
 
-            return celsat.generateKeyPair();
+    //duhet mi gjeneru celsat per me i ru n files
+    private KeyPair keyGenerator() throws Exception{
+        KeyPairGenerator celsat=KeyPairGenerator.getInstance("RSA");
+        celsat.initialize(1024);
+
+        return celsat.generateKeyPair();
     }
-  
-  public void createUser(){
+
+    public void createUser(){
         //Krijimi i celsave publik dhe privat
         try{
             validimi(this.emri);
@@ -47,7 +44,7 @@ public class CreateDeleteUser {
             KeyPair dyshjaCelesave=keyGenerator();
             PublicKey celesiPublik=dyshjaCelesave.getPublic();
             PrivateKey celesiPrivat=dyshjaCelesave.getPrivate();
-            
+
             KeyFactory kf=KeyFactory.getInstance("RSA");
             RSAPrivateCrtKeySpec rsaPrivate=kf.getKeySpec(celesiPrivat,RSAPrivateCrtKeySpec.class);
 
@@ -56,14 +53,35 @@ public class CreateDeleteUser {
             Document documentPrivate=documentBuilder.newDocument();
             Document documentPublic=documentBuilder.newDocument();
 
-            
+            String privateFile="src/PPKeys/keys/"+this.emri+".xml";
+            String publicFile="src/PPKeys/keys/"+this.emri+".pub.xml";
+
+            String modulusEncoded= Base64.getEncoder().encodeToString(rsaPrivate.getModulus().toString().getBytes());
+            String exponentEncoded=Base64.getEncoder().encodeToString(rsaPrivate.getPublicExponent().toString().getBytes());
+            String PrimeP=Base64.getEncoder().encodeToString(rsaPrivate.getPrimeP().toString().getBytes());
+            String PrimeQ=Base64.getEncoder().encodeToString(rsaPrivate.getPrimeQ().toString().getBytes());
+            String DP=Base64.getEncoder().encodeToString(rsaPrivate.getPrimeExponentP().toString().getBytes());
+            String DQ=Base64.getEncoder().encodeToString(rsaPrivate.getPrimeExponentQ().toString().getBytes());
+            String InverseQ=Base64.getEncoder().encodeToString(rsaPrivate.getCrtCoefficient().toString().getBytes());
+            String privateExponent=Base64.getEncoder().encodeToString(rsaPrivate.getPrivateExponent().toString().getBytes());
+
+            xmlCreator xml =new xmlCreator(privateFile,documentPrivate,modulusEncoded,exponentEncoded,PrimeP,PrimeQ,DP,DQ,InverseQ,privateExponent);
+            xmlCreator xmlPublic=new xmlCreator(publicFile,documentPublic,modulusEncoded,exponentEncoded);
+
+        } catch(NamingException e){
+            System.out.println("Emri i dhene nuk eshte valid\nEmri duhet te permbaje vetem shkronja,numra apo '_'");
+        } catch(FileAlreadyExistsException e){
+            System.out.println("File "+this.emri+" ekziston paraprakisht");
         } catch(Exception e){
             System.out.println(e.getMessage());
         }
     }
-  public void deleteUser(){
+
+    //duhet me bo funksion me i fshi perdoruesit
+    public void deleteUser(){
         Path pathPrivate= Path.of("src/PPKeys/keys/"+this.emri+".xml");
         Path pathPublic=Path.of("src/PPKeys/keys/"+this.emri+".pub.xml");
+
         try{
             validimi(this.emri);
             if(Files.exists(pathPrivate)) {
@@ -81,4 +99,5 @@ public class CreateDeleteUser {
             System.out.println("Gabim:Celesi '"+this.emri+"' nuk ekziston");
         }
     }
+
 }
