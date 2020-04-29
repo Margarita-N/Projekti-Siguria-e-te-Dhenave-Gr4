3593478cc11eed27a000c1bce6923ced94de89e0
Margarita-N <58574357+Margarita-N@users.noreply.github.com>
2020-04-29 12:50:44 +0200

    Shtimi i komandave te reja ne exceptionin e komandes kryesore

--- Siguria-e-te-Dhenave/Commands/MainCommandException.java
+++ Siguria-e-te-Dhenave/Commands/MainCommandException.java
@@ -1,9 +1,11 @@
-public class MainCommandException extends Exception {
-	public MainCommandException() {
-		super("Komanda e dhene eshte e pavlefshme!"+
-			 "\nKomandat e vlefshme jane keto:"+
-			 "\n1.rail-fence\n2.case\n3.numerical"+
-			 "\nKUJDES madhesine e shkronjave!");
-		
-	}
+package Komandat;
+
+public class MainCommandException extends Exception{
+    public MainCommandException() {
+        super("Komanda e dhene eshte e pavlefshme!"+
+                "\nKomandat e vlefshme jane keto:"+
+                "\n1.rail-fence\n2.case\n3.numerical\n4.create-user\n5.delete-user\n6.export-key\n7.import-key\n8.write-message\n9.read-message"+
+                "\nKUJDES madhesine e shkronjave!");
+
+    }
 }
