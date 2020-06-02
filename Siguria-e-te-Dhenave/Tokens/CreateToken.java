fe183753801b61368f7fa22613135aa2e18e23e8
Margarita-N <58574357+Margarita-N@users.noreply.github.com>
2020-06-02 15:34:57 +0200

    Parsimi i XML file per leximin e celesit privat

---
+++ Siguria-e-te-Dhenave/Tokens/CreateToken.java
@@ -0,0 +1,46 @@
+package Tokens;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigInteger;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.spec.RSAPrivateKeySpec;
+import java.security.spec.RSAPublicKeySpec;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Base64;
+import java.util.Date;
+
+public class CreateToken {
+    private String issuer;
+
+    public CreateToken(String issuer){
+        this.issuer=issuer;
+    }
+
+    public String generateToken(String issuer) {
+        try {
+            File xmlFile = new File("C:\\Users\\HP\\IdeaProjects\\jwt-excercises\\src\\main\\java\\PPKeys\\keys\\"+issuer+".xml");
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            Document document = documentBuilder.parse(xmlFile);
+
+            
+        } catch (FileNotFoundException e) {
+            System.out.println("Gabim:Nuk ekziston celesi privat " + issuer + ".xml");
+            return "";
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return "";
+        }
+    }
+}
